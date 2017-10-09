9d034f011683e96cd3b99e63cbd66ef60a451abc
mjburton <mjburton@mit.edu>
2017-10-09 10:43:52 -0400

    break aircraft into mission

--- STOL/stol.py
+++ STOL/stol.py
@@ -1,24 +1,42 @@
 from gpkit import Variable, Model
 
-class Aicraft(Model):
+class Aircraft(Model):
     def setup(self):
 
         W = Variable("W", "lbf", "aircraft weight")
         Wpay = Variable("W_{pay}", 500, "lbf", "payload weight")
         hbatt = Variable("h_{batt}", 300, "W*hr/kg", "battery specific energy")
         LoD = Variable("(L/D)", 10, "-", "lift to drag ratio")
-        g = Variable("g", 9.81, "m/s**2", "gravitational constant")
-        R = Variable("R", 50, "nmi", "aircraft range")
         etatotal = Variable("\\eta_{total}", 0.8, "-",
                             "total propulsive efficiency")
         Wbatt = Variable("W_{batt}", "lbf", "battery weight")
 
-        constraints = [R <= hbatt*Wbatt/W/g*LoD*etatotal,
-                       W >= Wbatt + Wpay]
+        constraints = [W >= Wbatt + Wpay]
 
         return constraints
 
+class Cruise(Model):
+    def setup(self, aircraft):
+
+        R = Variable("R", 50, "nmi", "aircraft range")
+        g = Variable("g", 9.81, "m/s**2", "gravitational constant")
+
+        constraints = [R <= (
+            aircraft["h_{batt}"]*aircraft["W_{batt}"]/aircraft["W"]/g
+            * aircraft["(L/D)"]*aircraft["\\eta_{total}"])]
+
+        return constraints
+
+class Mission(Model):
+    def setup(self):
+
+        aircraft = Aircraft()
+
+        cruise = Cruise(aircraft)
+
+        return aircraft, cruise
+
 if __name__ == "__main__":
-    M = Aicraft()
+    M = Mission()
     M.cost = M["W"]
     sol = M.solve("mosek")
